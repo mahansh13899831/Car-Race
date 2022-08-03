9ee9c27e6a3107b4af26d17eae2770ea69565cdf
mahan <96464043+mahansh13899831@users.noreply.github.com>
2022-08-03 20:12:21 +0430

    Create py-game-1.py
    باگ ر درست کردم

--- pygame/py-game-1.py
+++ pygame/py-game-1.py
@@ -37,7 +37,7 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     mouse=pygame.mouse.get_pos()
     click=pygame.mouse.get_pressed()
 
-    # print(click)
+     print(click)
 
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
